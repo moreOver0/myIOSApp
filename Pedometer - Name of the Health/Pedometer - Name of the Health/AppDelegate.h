c8280b79f884ecae9d57eeae47bc2649f40c3df2
my <398289931@qq.com>
2016-01-19 15:53:45 +0800

    Where We Begin

---
+++ Pedometer - Name of the Health/Pedometer - Name of the Health/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pedometer - Name of the Health
+//
+//  Created by my on 16/1/19.
+//  Copyright © 2016年 zhux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pedometer - Name of the Health/Pedometer - Name of the Health/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Pedometer - Name of the Health
+//
+//  Created by my on 16/1/19.
+//  Copyright © 2016年 zhux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
