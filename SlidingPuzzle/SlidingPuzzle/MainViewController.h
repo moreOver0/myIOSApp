fe822bab85700a417d22c66e5ffaddcc5360081b
my <398289931@qq.com>
2016-01-21 21:01:13 +0800

    main block completed

--- SlidingPuzzle/SlidingPuzzle/MainViewController.h
+++ SlidingPuzzle/SlidingPuzzle/MainViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-//#import "PuzzleView.h"
+
 @interface MainViewController : UIViewController
 
 
--- SlidingPuzzle/SlidingPuzzle/MainViewController.m
+++ SlidingPuzzle/SlidingPuzzle/MainViewController.m
@@ -10,10 +10,10 @@
 
 @interface MainViewController ()
 {
-    
-    int deltaY;
+    int N;
+    int blankPosX, blankPosY;
     UIImageView* imageViews[3][3];
-    UIImage* images[9];
+    UIImage* images[3][3];
     
 }
 
@@ -22,33 +22,96 @@
 
 @implementation MainViewController
 
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+
 - (void)initImages {
+    for(int i = 0; i < N; ++i)
+    {
+        for(int j = 0; j < N; ++j){
+            images[i][j] = [UIImage imageNamed:[NSString stringWithFormat:@"%d_%d-%d.jpg", N, i, j]];
+        }
+    }
+    images[blankPosX][blankPosY] = nil;
+}
+
+- (void)initImageViews {
+    CGFloat deltaY = self.navigationController.navigationBar.frame.size.height + self.navigationController.navigationBar.frame.origin.y;
+    CGFloat deltaX = 5;
+    CGFloat width = [[UIScreen mainScreen] bounds].size.width;
+    CGFloat height = [[UIScreen mainScreen] bounds].size.height - deltaY;
+
+    int interal = 2;
+    CGFloat blockLen = (width-8)/N - 2;
+    deltaY += (height-(blockLen*N+interal*(N-1)))/2;
     
+    CGFloat x = 0;
+    CGFloat y = 0;
+    for(int i = 0; i < N; ++i)
+    {
+        x = 0;
+        for(int j = 0; j < N; ++j)
+        {
+            imageViews[i][j] = [[UIImageView alloc] initWithImage:images[i][j]];
+            imageViews[i][j].frame = CGRectMake(x+deltaX, y+deltaY, blockLen, blockLen);
+            
+            [imageViews[i][j] setUserInteractionEnabled:YES];
+            UITapGestureRecognizer *singleTap =  [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(moveBlock:)];
+            [singleTap setNumberOfTapsRequired:1];
+            singleTap.delegate = imageViews[i][j];
+            [imageViews[i][j] addGestureRecognizer:singleTap];
+
+            x += interal + blockLen;
+        }
+        y += interal + blockLen;
+    }
+    imageViews[blankPosX][blankPosY].image = nil;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
-    deltaY = self.navigationController.navigationBar.frame.size.height + self.navigationController.navigationBar.frame.origin.y;
-    printf("%d\n", deltaY);
-    
-    
-    
-    self.view.backgroundColor = [UIColor lightGrayColor];
-    UIImageView* imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Maggie_3s.jpg"]];
-    imageView1.frame = CGRectMake(0, 64, 120, 120);
-    
-    
-    [imageView1 setUserInteractionEnabled:YES];
-    UITapGestureRecognizer *singleTap =  [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapping:)];
-    [singleTap setNumberOfTapsRequired:1];
-    [imageView1 addGestureRecognizer:singleTap];
-    
-    
-    [self.view addSubview:imageView1];
-    
+    N = 3;
+    blankPosX = N-1;
+    blankPosY = N-1;
+    [self initImages];
+    [self initImageViews];
 
+
+    self.view.backgroundColor = [UIColor lightGrayColor];
+
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < N; ++j){
+            [self.view addSubview:imageViews[i][j]];
+        }
+    }
+    if([self check]){
+        for(int i = 0; i < N; ++i){
+            for(int j = 0; j < N; ++j){
+                [imageViews[i][j] setUserInteractionEnabled:NO];
+            }
+        }
+    }
+    
+    UISwipeGestureRecognizer *swipe;
+    swipe =  [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeBlock:)];
+    [swipe setDirection:(UISwipeGestureRecognizerDirectionRight)];
+    [self.view addGestureRecognizer:swipe];
+    
+    swipe =  [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeBlock:)];
+    [swipe setDirection:(UISwipeGestureRecognizerDirectionUp)];
+    [self.view addGestureRecognizer:swipe];
+    
+    swipe =  [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeBlock:)];
+    [swipe setDirection:(UISwipeGestureRecognizerDirectionLeft)];
+    [self.view addGestureRecognizer:swipe];
+    
+    swipe =  [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeBlock:)];
+    [swipe setDirection:(UISwipeGestureRecognizerDirectionDown)];
+    [self.view addGestureRecognizer:swipe];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -67,8 +130,97 @@
 */
 
 
-- (void)singleTapping: (UIGestureRecognizer *)recognizer{
-    printf("get\n");
+- (bool)validPosX:(int)x PosY:(int)y {
+    if(x>=0 && x<N && y>=0 && y<N)
+        return true;
+    return false;
 }
+
+- (void)exchangePosX1:(int)x1 PosY1:(int)y1 PosX2:(int)x2 PosY2:(int)y2 {
+    
+    
+}
+
+- (void)exchangeBlankPosX:(int)x PosY:(int)y {
+    if([self validPosX:x PosY:y]){
+        UIImage* tmp = imageViews[x][y].image;
+        imageViews[x][y].image = imageViews[blankPosX][blankPosY].image;
+        imageViews[blankPosX][blankPosY].image = tmp;
+        blankPosX = x;
+        blankPosY = y;
+        printf("blank to %d %d\n", blankPosX, blankPosY);
+
+    }
+}
+
+- (bool)check {
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < N; ++j){
+            if(imageViews[i][j].image != images[i][j]){
+//                printf("%d %d\n", i, j);
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+- (void)moveBlock: (UIGestureRecognizer *)recognizer{
+    UIImageView* curr = (UIImageView*)recognizer.delegate;
+    int x = -1;
+    int y = -1;
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < N; ++j){
+            if(curr == imageViews[i][j]){
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    if(x != -1 && y != -1){
+        bool get = false;
+        for(int k = 0; k < 4; ++k){
+            if(x+dx[k] == blankPosX && y+dy[k] == blankPosY){
+                get = true;
+                break;
+            }
+        }
+        if(get){
+            [self exchangeBlankPosX:x PosY:y];
+        }
+    }
+    
+    if([self check]){
+        for(int i = 0; i < N; ++i){
+            for(int j = 0; j < N; ++j){
+                [imageViews[i][j] setUserInteractionEnabled:NO];
+            }
+        }
+    }
+}
+
+- (void)swipeBlock: (UISwipeGestureRecognizer *)recognizer {
+    if([self check])
+        return;
+    int x = blankPosX;
+    int y = blankPosY;
+    if(recognizer.direction == UISwipeGestureRecognizerDirectionLeft){
+        y++;
+    }
+    else if(recognizer.direction == UISwipeGestureRecognizerDirectionDown){
+        x--;
+    }
+    else if(recognizer.direction == UISwipeGestureRecognizerDirectionUp){
+        x++;
+    }
+    else if(recognizer.direction == UISwipeGestureRecognizerDirectionRight){
+        y--;
+    }
+    
+    [self exchangeBlankPosX:x PosY:y];
+}
+
+
 
 @end
